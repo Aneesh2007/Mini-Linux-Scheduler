e735a5f8736c289a3af033df68e134002d19003c
Aneesh2007 <Aneesh@Aneesh.localdomain>
2026-02-03 17:03:10 +0000

    inital project structure

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "scheduler.h"
+
+int main() {
+    printf("Mini Linux Scheduler Simulator\n");
+    return 0;
+}
---
+++ src/scheduler.h
@@ -0,0 +1,23 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#define MAX_PROCESSES 20
+#define MAX_GANTT 1000
+
+typedef struct {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int remaining_time;
+
+    int completion_time;
+    int turnaround_time;
+    int waiting_time;
+} Process;
+
+typedef struct {
+    int time;
+    int pid;
+} GanttEntry;
+
+#endif
