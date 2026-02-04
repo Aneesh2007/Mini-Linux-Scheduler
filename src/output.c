97212727411b7268882d830e25052dddfe2c6f61
Aneesh2007 <Aneesh@Aneesh.localdomain>
2026-02-04 06:17:50 +0000

    output file modified

--- src/output.c
+++ src/output.c
@@ -14,3 +14,26 @@ void print_gantt(GanttEntry gantt[], int len) {
     }
     printf("\n");
 }
+void print_process_table(Process p[], int n) {
+    float total_tat = 0, total_wt = 0;
+
+    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\n",
+               p[i].pid,
+               p[i].arrival_time,
+               p[i].burst_time,
+               p[i].completion_time,
+               p[i].turnaround_time,
+               p[i].waiting_time);
+
+        total_tat += p[i].turnaround_time;
+        total_wt += p[i].waiting_time;
+  }
+
+    printf("\nAverage Turnaround Time (ATAT): %.2f\n",
+           total_tat / n);
+    printf("Average Waiting Time (AWT): %.2f\n",
+           total_wt / n);
+}
