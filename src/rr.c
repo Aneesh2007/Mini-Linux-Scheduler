a11c2233d4a568ae4c82c8ef1ba6b636fe862ac4
Aneesh2007 <aneesh.bansal07@gmail.com>
2026-02-07 12:40:50 +0000

    rr rpaired and code completed

--- src/rr.c
+++ src/rr.c
@@ -7,59 +7,55 @@ void round_robin(Process p[], int n, int quantum,
     int time = 0, completed = 0;
     int queue[MAX_PROCESSES];
     int front = 0, rear = 0;
-    int in_queue[MAX_PROCESSES] = {0};
 
+    int visited[MAX_PROCESSES] = {0};
     *gantt_len = 0;
 
     while (completed < n) {
 
-        
+        /* Add newly arrived processes */
         for (int i = 0; i < n; i++) {
             if (p[i].arrival_time <= time &&
-                p[i].remaining_time > 0 &&
-                !in_queue[i]) {
-
+                !visited[i]) {
                 queue[rear++] = i;
-                in_queue[i] = 1;
+                visited[i] = 1;
             }
         }
 
-        
+        /* CPU idle */
         if (front == rear) {
             time++;
             continue;
         }
 
         int idx = queue[front++];
-        in_queue[idx] = 0;
 
         int exec_time =
             (p[idx].remaining_time < quantum)
             ? p[idx].remaining_time
             : quantum;
 
-        
         for (int t = 0; t < exec_time; t++) {
             gantt[*gantt_len].time = time;
             gantt[*gantt_len].pid = p[idx].pid;
             (*gantt_len)++;
-            time++;
 
-            
+            time++;
+            p[idx].remaining_time--;
+
+            /* Add processes that arrive during execution */
             for (int i = 0; i < n; i++) {
                 if (p[i].arrival_time <= time &&
-                    p[i].remaining_time > 0 &&
-                    !in_queue[i]) {
-
+                    !visited[i]) {
                     queue[rear++] = i;
-                    in_queue[i] = 1;
+                    visited[i] = 1;
                 }
             }
+
+            if (p[idx].remaining_time == 0)
+                break;
         }
 
-        p[idx].remaining_time -= exec_time;
-
-        
         if (p[idx].remaining_time == 0) {
             p[idx].completion_time = time;
             p[idx].turnaround_time =
@@ -68,9 +64,7 @@ void round_robin(Process p[], int n, int quantum,
                 p[idx].turnaround_time - p[idx].burst_time;
             completed++;
         } else {
-            
-            queue[rear++] = idx;
-            in_queue[idx] = 1;
+            queue[rear++] = idx;  // rotate
         }
     }
 }
